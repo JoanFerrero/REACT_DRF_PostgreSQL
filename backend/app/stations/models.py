e068013bf92667fbe1a3ca2d2c6c8d7038ce2da8
JoanFerrero <joan.ferrero.montiel@gmail.com>
2024-01-17 17:52:13 +0100

    Add models Stations, train and chair

--- backend/app/stations/models.py
+++ backend/app/stations/models.py
@@ -2,9 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class Stations(models.Model):
+class Station(models.Model):
     #slug = models.SlugField(max_length=100, unique=True, editable=False)
     slug = models.CharField(max_length=100, unique=True, editable=False)
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.id)
+
+class Train(models.Model):
+    slug = models.CharField(max_length=100, unique=True, editable=False)
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.id)
+
+class Chair(models.Model):
+    slug = models.CharField(max_length=100, unique=True, editable=False)
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.id)
